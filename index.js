bdf873fa80c10365bceb0e8b74d2c5b7c9bd55fa
Nataliya R <n.rudenko@gmail.com>
2019-09-17 17:18:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function justInvoke(fn) {
   return fn();
- };
+ }
  
  function setThisWithCall(fn, thisValue, arg){
    return fn.call(thisValue, arg);
- };
+ }
  
  function setThisWithApply(fn, thisValue, args){
    return fn.apply(thisValue, [...args]);
- };
+ }
   function returnNewFunctionOf(functionToBeCopied, thisValue){
     return functionToBeCopied.bind(thisValue);
   }
